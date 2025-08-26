55a6deb293ac22c50282ec4947068fab5083b83d
remi.germe <remi.germe@gmail.com>
2025-08-26 12:53:09 -0700

    tests hand

--- src/hand.rs
+++ src/hand.rs
@@ -1,4 +1,4 @@
-use std::ops::Deref;
+use std::{collections::HashSet, ops::Deref};
 
 use crate::card::Card;
 
@@ -17,6 +17,16 @@ impl Deref for Hand {
 
 impl From<Vec<Card>> for Hand {
     fn from(value: Vec<Card>) -> Self {
+        for c in &value {
+            if !c.is_valid() {
+                panic!("Creating a hand with an invalid card: {:?}", c);
+            }
+        }
+
+        if value.len() != value.iter().copied().collect::<HashSet<Card>>().len() {
+            panic!("Creating a hand with duplicate cards: {:?}", value);
+        }
+
         Hand { cards: value }
     }
 }
@@ -29,5 +39,36 @@ impl Hand {
         } else {
             self.cards.clone()
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_hand_with_invalid_card() {
+        let _hand: Hand = vec![Card::Blue(0)].into();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_hand_with_duplicates() {
+        let _hand: Hand = vec![Card::Blue(1), Card::Blue(1)].into();
+    }
+
+    #[test]
+    fn test_playable_cards() {
+        let hand: Hand = vec![Card::Blue(1), Card::Blue(2), Card::Red(5), Card::Trump(3)].into();
+
+        assert_eq!(
+            hand.playable_cards(&Card::Blue(5)),
+            vec![Card::Blue(1), Card::Blue(2)]
+        );
+        assert_eq!(hand.playable_cards(&Card::Red(3)), vec![Card::Red(5)]);
+        assert_eq!(hand.playable_cards(&Card::Trump(4)), vec![Card::Trump(3)]);
+
+        assert_eq!(hand.playable_cards(&Card::Yellow(5)), *hand);
     }
 }
