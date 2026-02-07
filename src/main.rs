3d1152d45d8a7db151809bfd149a368f9a85252f
remi.germe <remi.germe@gmail.com>
2026-02-07 15:19:08 +0100

    adapting examples

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let mut p1 = Player::new(vec![Card::Submarine(4), Card::Blue(2)].into());
     let p2 = Player::new(vec![Card::Blue(1), Card::Pink(6)].into());
 
-    p1.add_task(TaskWinCards::new([Card::Blue(1), Card::Pink(6)]));
+    p1.add_task(TaskWinCards::new(None, [Card::Blue(1), Card::Pink(6)]));
 
     let mut s = State::new(vec![p1, p2]);
     match s.play() {
@@ -24,30 +24,32 @@ fn main() {
 
     // Intermediate demo
     let mut state = State::new_random(4);
-    state.get_mut_player(0).add_task(TaskWinCards::new([
-        Card::Blue(1),
-        Card::Pink(6),
-        Card::Green(9),
-        Card::Yellow(4),
-    ]));
+    state.get_mut_player(0).add_task(TaskWinCards::new(
+        None,
+        [
+            Card::Blue(1),
+            Card::Pink(6),
+            Card::Green(9),
+            Card::Yellow(4),
+        ],
+    ));
     state
         .get_mut_player(1)
-        .add_task(TaskDontWinCards::new_from_colors([
-            Card::Blue as fn(usize) -> Card,
-            Card::Green,
-        ]));
-    state
-        .get_mut_player(2)
-        .add_task(TaskDontOpenTrickWith::new([
-            Card::Blue as fn(usize) -> Card,
-            Card::Pink,
-        ]));
+        .add_task(TaskDontWinCards::new_from_colors(
+            None,
+            [Card::Blue as fn(usize) -> Card, Card::Green],
+        ));
+    state.get_mut_player(2).add_task(TaskDontOpenTrickWith::new(
+        None,
+        [Card::Blue as fn(usize) -> Card, Card::Pink],
+    ));
     state
         .get_mut_player(3)
-        .add_task(TaskDontWinCards::new_from_values([5, 8]));
-    state
-        .get_mut_player(3)
-        .add_task(TaskDontWinCards::new([Card::Yellow(1), Card::Yellow(2)]));
+        .add_task(TaskDontWinCards::new_from_values(None, [5, 8]));
+    state.get_mut_player(3).add_task(TaskDontWinCards::new(
+        None,
+        [Card::Yellow(1), Card::Yellow(2)],
+    ));
     match state.play() {
         Ok(solution) => println!("Found a solution: {:?}", solution),
         Err(GameError::NoSolutionFound) => println!("Unfortunately this game is not feasible"),
@@ -65,10 +67,10 @@ fn main() {
     //     ]
     //     .into(),
     // );
-    // r.add_task(TaskDontOpenTrickWith::new([
-    //     Card::Green as fn(usize) -> Card,
-    //     Card::Pink,
-    // ]));
+    // r.add_task(TaskDontOpenTrickWith::new(
+    //     None,
+    //     [Card::Green as fn(usize) -> Card, Card::Pink],
+    // ));
     // let mut m = Player::new(
     //     [
     //         Card::Submarine(4),
@@ -79,8 +81,12 @@ fn main() {
     //     ]
     //     .into(),
     // );
-    // m.add_task(TaskWinTrickWithPred::new_odd());
-    // m.add_task(TaskWinCardsAmountColor::new_at_least_5_pink());
+    // m.add_task(TaskWinTrickWithPred::new_odd(None));
+    // m.add_task(TaskWinCardsAmountColor::new(
+    //     None,
+    //     false,
+    //     [(Card::Pink as fn(usize) -> Card, 5)],
+    // ));
     // m.add_trick(
     //     (
     //         3,
@@ -99,7 +105,12 @@ fn main() {
     //     (
     //         4,
     //         0,
-    //         [Card::Pink(7), Card::Pink(8), Card::Pink(9), Card::Submarine(2)],
+    //         [
+    //             Card::Pink(7),
+    //             Card::Pink(8),
+    //             Card::Pink(9),
+    //             Card::Submarine(2),
+    //         ],
     //     )
     //         .into(),
     // )
@@ -114,12 +125,15 @@ fn main() {
     //     ]
     //     .into(),
     // );
-    // f.add_task(TaskWinCards::new([
-    //     Card::Blue(3),
-    //     Card::Pink(3),
-    //     Card::Green(3),
-    //     Card::Yellow(3),
-    // ]));
+    // f.add_task(TaskWinCards::new(
+    //     None,
+    //     [
+    //         Card::Blue(3),
+    //         Card::Pink(3),
+    //         Card::Green(3),
+    //         Card::Yellow(3),
+    //     ],
+    // ));
     // f.add_trick(
     //     (
     //         0,
@@ -167,9 +181,10 @@ fn main() {
     //     ]
     //     .into(),
     // );
-    // l.add_task(TaskDontWinCards::new_from_colors([
-    //     Card::Submarine as fn(usize) -> Card
-    // ]));
+    // l.add_task(TaskDontWinCards::new_from_colors(
+    //     None,
+    //     [Card::Submarine as fn(usize) -> Card],
+    // ));
     // let mut state = State::new([m, l, r, f]);
     // state.current_trick = (5, 0, []).into(); // You need to make it public to run this example
     // match state.play() {
